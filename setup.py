693c655038fe0a3fa73e7f937f3c2053c919965f
Will Stevens <wstevens@cloudops.com>
2012-03-30 14:59:56 -0400

    updated the package details

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
+# Copyright (c) 2011-2012 CloudOps
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from setuptools import setup, find_packages
+from swift_usage import __version__ as version
+
+name = 'cs_auth'
 
 setup(
-    name='cs_auth',
-    version='0.1',
+    name=name,
+    version=version,
     description='Swift middleware to authenticate against CloudStack',
+    license='Apache License (2.0)',
     classifiers=['Programming Language :: Python'],
     keywords='cs_auth auth authentication openstack cloudstack',
     author='CloudOps / Will Stevens (swill)',
