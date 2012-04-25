b6aec4f88a34f3bff03f1e80dab8bb2f76363f22
Will Stevens <wstevens@cloudops.com>
2012-04-25 13:24:18 -0400

    updated version

--- cs_auth/__init__.py
+++ cs_auth/__init__.py
@@ -1,4 +1,4 @@
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (0, 0, 3)
+version_info = (0, 0, 4)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
